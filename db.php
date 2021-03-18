621b85d94cb8abcd387f19b69bbeb1aba0a6696e
Jonas <git@it-jonas.com>
2021-03-18 16:31:54 +0100

    add example db file

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+# this are example values. please change if you want to run this software
+$host = '127.0.0.1';
+$port = '3306';
+$dbname = 'kontaktverfolgung';
+$tablename = 'kontaktverfolgung_tbl';
+$dbuser = 'kontaktUser';
+$dbpw = 'verysafepassword!!!!';
+?>
