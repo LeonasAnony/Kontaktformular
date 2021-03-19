f520617f05d31598f872ac19dc760cbe1bfcde80
Leonas <29leon42005@gmaii.com>
2021-03-19 10:03:50 +0100

    Started rewriting
    Changing the form so that the DATETIME is filled by script and user can only logout via Code

--- formular.php
+++ formular.php
@@ -20,9 +20,6 @@ if (isset($_GET['data'])) {
   $name = trim($_POST['name']);
   $email = trim($_POST['email']);
   $tel = trim($_POST['tel']);
-  $anreise = trim($_POST['andate']);
-  $abreise = trim($_POST['abdate']);
-  $dauer = trim($_POST['dauer']);
 
   // Error wenn kein Name angegeben wurde
   if ($name == null) {
@@ -100,25 +97,16 @@ if (isset($_GET['data'])) {
         <form id="BX" action="?data=true" method="post">
           <h1>Kontakt-</h1>
           <h1>formular</h1>
-          <p1>Daten wurden verschlüsselt gespeichert<br/></p1>
-          <p3>Daher du dich ohne Abreise Datum eingetragen hast nutze bitte diesen Code:<b> <?php if(isset($code)){echo $code;}?></b> um dich <a href="logout.php">hier</a> auzutragen sobald du gehst.</p3>
+          <p1>Daten wurden verschlüsselt gespeichert, nutze bitte diesen Code:<b> <?php if(isset($code)){echo $code;}?></b> um dich <a href="logout.php">hier</a> auzutragen sobald du gehst.</p1>
           <label for="Nachname"><input type="text" size="40" maxlength="50" name="name" placeholder="Nachname"></label>
           <label for="Email"><input type="email" size="40" maxlength="250" name="email" placeholder="Email oder"></label>
           <label for="Telefonnummer"><input type="tel" size="30" name="tel" placeholder="Telefonnummer" pattern="^(\+[0-9]{2}|[0]{2}|01)[0-9]{8,20}$"></label>
-          <p2>Anreise:</p2>
-          <label for="Anreise"><input type="date" size="40" name="andate" placeholder="Anreise"></label>
-          <p2>Abreise:</p2>
-          <label for="Abreise"><input type="date" size="40 "name="abdate" placeholder="Abreise"></label>
-          <p2>oder:<br/></p2>
-          <p4>Ich weiß noch nicht wann ich wieder gehe: </p4><label for="Weiß noch nicht wann ich gehe"><input type="checkbox" name="dauer"></label>
+          <p4>Ich bin mit der Verarbeitung meiner Daten zu zwecken der Kontaktverfolgung im Falle einer Infektion im Camp einverstanden: </p4><label for="Ich bin mit der Verarbbeitung meiner Daten einverstanden"><input type="checkbox" name="einverständnis" required></label>
           <p>E-Mail oder Telefonnummer muss ausgefüllt sein<br/></p>
           <p5>Bitte eine gültige Email angeben<br/></p5>
           <p6>Bitte eine gültige Telefonnummer angeben<br/></p6>
           <p7>Bitte einen Namen angeben<br/></p7>
-          <p8>Bitte ein Anreisedatum angeben<br/></p8>
-          <p9>Bitte ein Abreisedatum angeben oder den Haken aktivieren<br/></p9>
           <p10>Beim Abspecheichern ist ein Fehler aufgetreten. Bitte versuche es erneut. Wenn das Problem weiterhin besteht ende dich an T:@Le0nas<br/></p10>
-          <p11>Abreise darf nicht vor Anreise stattfinden<br/></p11>
           <input type="submit" value="Speichern">
           <a href="https://bremen.klimacamp.eu">Klimacamp</a>
         </form>
